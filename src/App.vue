6e31bfe210f0b27fb7c450786743d0dcb95a55e4
Saran Pazany <pazanysaran@gmail.com>
2024-10-31 15:09:37 +0700

    TP1: Finish exercise 4.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
-<script setup>
-
-</script>
-
 <template>
-  This is my first Vue JS project.
-
-  <RouterView />
-</template>
-
-<style scoped>
-
-</style>
+  <div>
+ 
+   <Categories/>
+   <Banners/>
+ 
+   </div>
+ 
+  
+   <RouterView />
+ </template>
+ 
+ <script>
+ import Categories from "./views/Categories.vue";
+ import Banners from "./views/Banners.vue";
+ 
+ export default {
+   components: {
+     Categories,
+     Banners,
+    
+   },
+ 
+   data() {
+     return {
+      
+ 
+     
+     };
+   },
+ };
+ </script>
+ 
+ <style scoped>
+ *{
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+   color:black;
+   
+ }
+ body{
+   background-color: rgb(198, 198, 198);
+ }
+ 
+ </style>
+ 
---
+++ src/views/Banners.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="promotion-banner-list">
+    <Promotions
+      v-for="(banner, index) in banners"
+      :key="index"
+      :title="banner.title"
+      :description="banner.description"
+      :imageSrc="banner.imageSrc"
+      :bgColor="banner.bgColor"
+      :bgBtnColor="banner.bgBtnColor"
+      :label="banner.label" />
+  </div>
+</template>
+<script>
+import Promotions from "@/components/Promotions.vue";
+export default {
+  components: { Promotions },
+  data() {
+    return {
+      banners: [
+        {
+          title: "Everyday Fresh & Clean with Our Products",
+          description: "Try it!",
+          imageSrc: "./public/images/union.jpg",
+          bgColor: "#F0E8D5",
+          bgBtnColor: "#36b37e",
+          label: "Shop Now",
+        },
+        {
+          title: "Make your Breakfast Healthy and Easy",
+          description: "Try it!",
+          imageSrc: "./public/images/milk.png",
+
+          bgColor: "#F3E8E8",
+          bgBtnColor: "#36b37e",
+          label: "Shop Now",
+        },
+        {
+          title: "The best Organic Products Online",
+          description: "Try it!",
+          imageSrc: "./public/images/vegetable.jpg",
+          bgColor: "#E7EAF3",
+          bgBtnColor: "#36b37e",
+          label: "Shop Now",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.promotion-banner-list {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  /* justify-content: space-around; */
+  /* padding: 20px; */
+  /* background-color: #f0f2f5; */
+}
+</style>
